323ae4362da49cf55f7ae2997d66a62b3db19d15
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-09-22 00:16:49 -0700

    alslo exporting the array and objects

---
+++ 3-alternative-favour.js
@@ -0,0 +1,7 @@
+module.exports.items =['item ','item 2']
+const obj={
+    name:'bob',
+    age:20,
+}
+
+module.exports.singleobj=obj
--- main-app.js
+++ main-app.js
@@ -3,6 +3,9 @@
 //importing the name and function form the directory of nmae and funcion
 const names =require(`./1-names`)
 const sayhi=require(`./2-sayhi`)
+const data=require('./3-alternative-favour')
+
+console.log(data.items[0])
 
 sayhi(names.susan)
 sayhi(names.jhon)
