387f439c44d6c80851ff0edfcf42bc12cc32b230
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-09-27 02:05:04 -0700

    node fundamental done

--- practice-file.js
+++ practice-file.js
@@ -3,8 +3,16 @@ const http=require('http')
 
 const server=http.createServer(
     (req,res)=>{
-        const text=fs.readFileSync('./content/result-file.txt','utf8')
-        res.end(text)
+        // const text=fs.readFileSync('./content/result-file.txt','utf8')
+        // res.end(text)
+    
+    const text= fs.createReadStream('./content/result-file.txt','utf8')
+    text.on('open',()=>{
+        text.pipe(res)
+    })
+    text.on('error',(err)=>{
+        res.end(err);
+    })
     }
 );
 
