0e35f5a8fc608e6a939e1af92433ab3d57af5aea
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-10-03 23:21:46 -0700

    created server serve other file

--- Node-express/app.js
+++ Node-express/app.js
@@ -1,18 +1,27 @@
 console.log("Express Tutorial");
 const https=require('http');
 const server=https.createServer((req,res)=>{
-const url =req.url;
+const {readFileSync}=require('fs')
 
+const homePage=readFileSync('./index.html')
+
+    //req.method
+
+const url =req.url;
+//home page
 if (url=='/'){    
 res.writeHead(200,{'content-type': 'text/html'});
-    res.write('<h1> home page </h1>');
+    res.write(homePage);
     res.end();
 }
+//about page 
  else if (url=='/about'){    
 res.writeHead(200,{'content-type': 'text/html'});
     res.write('<h1> About  page </h1>');
     res.end();
 }
+
+//404 page not found 
  else{    
 res.writeHead(404,{'content-type': 'text/html'});
     res.write('<h1> Not Found DATA </h1>');
