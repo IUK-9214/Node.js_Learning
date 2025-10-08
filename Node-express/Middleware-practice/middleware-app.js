3af672102cb94dcb53bd8a3539f4d1a923e46f5e
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-10-08 08:53:16 -0700

    completed
    middleware completed also installed morgan npm

--- Node-express/Middleware-practice/middleware-app.js
+++ Node-express/Middleware-practice/middleware-app.js
@@ -2,14 +2,20 @@ const express=require('express');
 const app=express();
 const logger=require('./logger')
 const authorize=require('./authorize')
+const morgan=require('morgan')
 //req => middleware => res
 //middle ware is a function which is run when sever hit the req the it start and run give res
 //here is the middle ware when i hit the request so it start the logger
 //aslo there is the next which is for the another middle ware
 //if we pass multi middleware so it would be in array form 
-app.use([authorize,logger])
+// app.use([authorize,logger])
 //by useing the use funcion it is calling it when ever a request is made 
 //also order matter i should use the teh use function before any request 
+
+
+
+//third party morgan npm 
+app.use(morgan('tiny'))
 
 app.get('/',(req,res)=>{
     res.status(200).send("home page ")
@@ -21,7 +27,8 @@ app.get('/about',(req,res)=>{
 app.get('/api/product',(req,res)=>{
     res.status(200).send("Products");
 })
-app.get('/api/items',(req,res)=>{
+app.get('/api/items',[authorize,logger],(req,res)=>{
+    console.log(req.user)
     res.status(200).send(" Items");
 })
 
