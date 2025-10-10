4c66d1ed1205311c4caabe8b54d9b84272f1eb06
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-10-10 06:09:31 -0700

    post method
    i use the post man to check the api
    using post method i send data  and it with the array of data from where is fetch the data

--- Node-express/Get_method/Get-method.js
+++ Node-express/Get_method/Get-method.js
@@ -21,6 +21,15 @@ res.status(201).json({success:true,person:name})
 
 })
 
+app.post('/api/postman/people',(req,res)=>{
+    const{name}=req.body
+    if(!name){
+        res.status(400).json({success:false,msg:"pleaze provide name value"})
+    }
+res.status(201).json({success:true,data:[...people,name]})
+
+})
+
 
 //post method
 app.post("/login",(req,res)=>{
